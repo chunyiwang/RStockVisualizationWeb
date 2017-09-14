431d9280bae7c249f406bbbf7cba76071647b2f8
chunyiwang <chunyiwang@v1020-wn-36-236.campus-dynamic.uwaterloo.ca>
2017-09-14 12:02:04 -0400

    first r implementation for stock

---
+++ RStock.R
@@ -0,0 +1,99 @@
+
+A visualization practice of Stock in R using shiny
+```{r}
+if (!require(quantmod)) {
+  stop("This app requires the quantmod package. To install it, run 'install.packages(\"quantmod\")'.\n")
+}
+
+
+## use quantmod
+library(shiny)
+library(shinydashboard)
+library(quantmod)
+library(TTR)
+library(gridExtra)
+library(grid)
+library(DT)
+symbols <- stockSymbols()
+symbols <- symbols[,1]
+
+
+
+choices <-
+  c(
+    "Market Cap","Enterprise Value",
+    "Trailing P/E"
+  )
+
+indicators <-
+  c(
+    "Bollinger Bands",
+    "Weighted Moving Average",
+    "Commodity Channel Index"
+  )
+
+#get data
+if (interactive()) {
+  options(device.ask.default = FALSE)
+  ui <- dashboardPage(
+    dashboardHeader(title = "Stock Data dashboard"),
+    dashboardSidebar(
+      sidebarMenu(
+        menuItem(
+          "Stock",
+          tabName = "Stock"
+        )
+      )),
+    dashboardBody(
+      titlePanel("Visualize Stock Data"),
+      sidebarLayout(
+        sidebarPanel(
+          dateRangeInput("Range", "Choose Date Range:", start=Sys.Date() - 365,
+                         end= Sys.Date(), format = "yyyy-mm-dd"),
+          selectizeInput('Name', label= 'Choose a stock', symbols, selected = NULL, multiple = FALSE,
+                         options = NULL),
+          selectInput("Color", label = "Select Color", choices = c("white","black")),
+          checkboxGroupInput("quickstats", "Key Statistics:",
+                             choices),
+          checkboxGroupInput("indicators", "Key Indicators:",
+                             indicators),
+          selectInput("PlotType", label = "Plot type", choices = c("candlesticks", "matchsticks", "bars", "line")),
+          submitButton("Submit")
+        ),
+        
+        mainPanel(
+          tabsetPanel(type="tab",tabPanel("Plot",plotOutput("Plot")),
+                      tabPanel("Statistics",tableOutput("Stat")),
+                    tabPanel("Return",DT::dataTableOutput("Return")))
+          
+        )
+      )
+    )
+  )
+  server <- function(input, output) {
+    output$Plot <- renderPlot({
+      chartSeries(get(getSymbols(input$Name)), type = input$PlotType, 
+                  theme=chartTheme(input$Color), name=paste(start, end,sep = " "), TA="addBBands();addEMA()")
+      zoomChart(paste(input$Range, collapse = "::"))
+    })
+    output$Return <- DT:: renderDataTable({
+      DT::datatable(
+        datatest <- periodReturn(get(getSymbols(input$Name)),by=years,from='2003-01-01',to='2004-01-01')
+
+        
+      )
+    })
+    observe({
+      print(input$Range)
+      print(input$Name)
+      print(input$Tools)
+      print(periodReturn(get(getSymbols(input$Name)),by=years,from='2003-01-01',to='2004-01-01'))
+    })
+  }
+  shinyApp(ui, server)
+}
+
+
+
+
+```
